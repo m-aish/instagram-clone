571f284aa7936bc5130458f4e5f955458815444e
Muthu Aiswaryaa Swaminathan <withlove.aish@gmail.com>
2021-05-01 21:10:31 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+const Login = lazy(() => import('./pages/login'))
+
 function App() {
-  return <p>Hello</p>;
+  return (
+    <Router>
+      <Switch>
+        <Suspense fallback={<p>Hold on for a sec!</p>}>
+          <Route path={ROUTES.LOGIN} component={Login} />
+        </Suspense>
+      </Switch>
+
+    </Router>
+  )
 }
 export default App;
